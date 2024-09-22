9d5b040d8f1ac97c195eece29fc78ce2e4746f72
Wandson Emanuel <107449320+Wuel1@users.noreply.github.com>
2024-09-22 13:44:35 -0300

    bhaskara

--- teste.hs
+++ teste.hs
@@ -34,3 +34,16 @@ main = putStrLn (show(cubo 5))
 
 soma3 :: Double -> Double -> Double -> Double
 soma3 s1 s2 s3 = s1 + s2 + s3
+
+{- Essa função é do tipo Double -}
+
+{-4. Escreva uma função em Haskell para encontrar o valor da expressão quadrática `ax^2 + bx + c` para os parâmetros `a`, `b`, `c` e `x`. Qual é o tipo dessa função? -}
+
+delta :: Float -> Float -> Float -> Float
+delta a b c = b**2 - 4 * a * c
+
+bhaskara :: Float -> Float -> Float -> (Float,Float)
+bhaskara a b c = 
+    let x1 = (-b + sqrt((delta a b c)))/2*a
+        x2 = (-b - sqrt((delta a b c)))/2*a
+    in (x1, x2)
